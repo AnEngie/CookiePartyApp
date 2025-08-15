bb7cdaf2d504d27464d5f39c46e4082794b3f146
Sed <sed.van.hesselingen@hva.nl>
2025-08-15 21:25:38 +0200

    Added Topbar to screen

--- app/src/main/java/nl/anengie/cookieparty/ui/screens/BuzzerScreen.kt
+++ app/src/main/java/nl/anengie/cookieparty/ui/screens/BuzzerScreen.kt
@@ -1,22 +1,59 @@
 package nl.anengie.cookieparty.ui.screens
 
+import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.PaddingValues
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.ThumbUp
+import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.Icon
+import androidx.compose.material3.IconButton
 import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.MaterialTheme.typography
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
+import androidx.compose.material3.TopAppBar
+import androidx.compose.material3.TopAppBarColors
+import androidx.compose.material3.TopAppBarDefaults
+import androidx.compose.material3.TopAppBarDefaults.topAppBarColors
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.unit.dp
 import androidx.navigation.NavController
+import nl.anengie.cookieparty.R
 
+@OptIn(ExperimentalMaterial3Api::class)
 @Composable
 fun BuzzerScreen(navController: NavController) {
     Scaffold(
+        topBar = {
+            TopAppBar(
+                title = {
+                    Column(
+                        modifier = Modifier.fillMaxWidth(),
+                        horizontalAlignment = Alignment.CenterHorizontally
+                    ) {
+                        Text(
+                            stringResource(id = R.string.app_name),
+                            color = Color.White,
+                            style = typography.headlineMedium
+                        )
+                    }
+                },
+                colors = topAppBarColors(
+                    containerColor = Color.Red
+                )
+            )
+        },
         content = { innerPadding ->
             Modifier.padding(innerPadding)
             ScreenContent(innerPadding, navController)
@@ -41,7 +78,7 @@ private fun ScreenContent(paddingValues: PaddingValues, navController: NavContro
         ) {
             Text(
                 text = "Hello World!",
-                style = MaterialTheme.typography.headlineMedium
+                style = typography.headlineMedium
             )
         }
     }
